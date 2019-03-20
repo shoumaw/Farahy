7c7353330c6576d3e9e9386a486a09383a93880b
Shouman <35404894+shoumaw@users.noreply.github.com>
2019-03-20 15:41:53 +0200

    Added my project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+var config = {
+  apiKey: "AIzaSyAPy71UBA75u3nHgfl4DvAOBu8Ckg1MZ8w",
+  authDomain: "farahy-ad87a.firebaseapp.com",
+  databaseURL: "https://farahy-ad87a.firebaseio.com",
+  projectId: "farahy-ad87a",
+  storageBucket: "farahy-ad87a.appspot.com",
+  messagingSenderId: "1072114452677"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase 
